94c108e58ca9877b89661c53b9c56aa49506603b
Morgan Hayes <jmorganh@users.noreply.github.com>
2016-10-12 16:20:19 -0600

    Update auth0-variables.js

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='o8zuX4w6SJH5Tt44CLUVDBNZKeZGErCS';
-var AUTH0_DOMAIN='tcsmaster.auth0.com';
+var AUTH0_CLIENT_ID='<clienID>';
+var AUTH0_DOMAIN='<domain>';
 var AUTH0_CALLBACK_URL=location.href;
